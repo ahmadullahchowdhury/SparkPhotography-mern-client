66a88c565f5f027021c5f71480fcad18415acf6a
kowchergit config --global user.email kowcher99@gmail.com <37480985+kowcher99@users.noreply.github.com>
2022-11-09 00:03:31 +0600

    home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-           <h1>This is home page</h1> 
+           <h1 className='text-3xl font-bold text-center p-4' >Welcome to Spark Photography</h1> 
+           <h1 className='text-xl font-bold text-center p-4' >Here I provide all kinds of photography service</h1> 
+           <div className='flex justify-center'>
+            
+           <img  src='https://cdn.pixabay.com/photo/2015/02/15/09/30/photography-636973_960_720.jpg' alt='banner' ></img>
+           </div>
+
+           <h1 className='text-3xl font-bold text-center p-4' >Explore List of services</h1> 
+           <div className='flex justify-center'>
+
+           <button className='btn btn-primary '>See All </button>
+           </div>
         </div>
     );
 };
